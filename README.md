c09d119e8997c5faf5d49fd306abfd097bfb0cff
corndog-labs <56854559+corndog-labs@users.noreply.github.com>
2021-03-04 01:01:31 -0800

    Saving project with assets
    saving

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # Project_1_Crother
  First Project in Interaction design 
 
- #OverVeiw
+#Overveiw
+
+In this project I expanded my knowlegde of state machines and arrays.
+The source code comes from the Rooms Of the House repository. 
+
+ file that describes the overview of the repository, which will include at least 1 paragraph of technical information about the source code AND the link to your Adobe XD document shared 
+
+
+##Adobe XD Link 
+
+https://xd.adobe.com/view/29f2eea5-ebed-4d9b-a718-6bf206db96b2-7ca1/
 
--- sketch.js
+++ sketch.js
@@ -1,36 +1,7 @@
 /***********************************************************************************P
 Project 1 
-	by Courtney Crother
-
-  Shows navigation structure using the keyboard between 4 rooms
-
-	(1) Add your own PNG files in the assets folder. Make sure they match the names ***exactly*** of the existing PNGs.
-	(2) Add custom drawing code to drawOne(), drawTwo(), drawThree(), drawFour(), drawFive()
-	(3) You can add your own interfaces - keys, mouse events, etc in the Interfaces section
-
-	Also start your localhost before running this, otherwise no PNGs will display
-
-------------------------------------------------------------------------------------
-	The way it works — you don't need to know this for the template use
-	* array of images gets loaded at startup
-	* drawFunction is a VARIABLE that points to a function varible name
-	* drawOne(), drawTwo(), etc. are set to be functions.
-	* the the keys 1-5 will change the drawFunction variable
-
-------------------------------------------------------------------------------------
-	Notes:
-	- a more advanced state machine with use array-indexing for each of
-		images the draw functions, but this is just for illustrative purposes
-
-	- even more advanced will be to put the draw functions into an array, would
-		be helpful for randomizing, go to the next function, etc
-
-	- next step after that would be to put interfaces into an array that maps to
-		the functions
-
-
+by Courtney Crother
 ***********************************************************************************/
-
 
 // variable that is a function 
 var drawFunction;
@@ -44,10 +15,39 @@ var midX
 var startY;
 var lineHeight = 24;
 
+//Nav box variables 
+var navX = 780; 
+var navY = 520;
+var navH = 60;
+var navW = 60;
+
+var navYb = 600; 
+var navYc = 680; 
+
+//Nav text variables 
+var navNumx = 810;
+var navNumy = 557;
+var navNumb = 639;
+var navNumc = 717;
+
+var statueAssests = [];
+
+// preload 
+
+function preload () {
+
+statueAssests[0] = loadImage('assets/wingedhell.png');
+statueAssests[1] = loadImage('assets/hell.png');
+statueAssests[2] = loadImage('assets/MiloHell.png');
+statueAssests[3] = loadImage('assets/k1archaic.png');
+statueAssests[4] = loadImage('assets/k2archaic.png');
+statueAssests[5] = loadImage('assets/davidClass.png');
+
+}
 
 // Center drawing, drawFunction will be one for default
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(900, 800);
 
   // Center our drawing objects
   imageMode(CENTER);
@@ -58,85 +58,192 @@ function setup() {
   drawFunction = drawEntrance;
 }
 
-// Very simple, sets the background color and calls your state machine function
 function draw() {
   background(0);
-
   fill(0);
-  
-  // will call your state machine function
   drawFunction();
 }
 
-//========= TEMPLATE: modify these functions, INSIDE the function blocks only =========
 
-//-- drawOne() will draw the image at index 0 from the array
 drawEntrance = function() {
-   //image(images[0],width/2, height/2);
+
+   fill(186, 85, 211);
+   textSize(50);
+   textFont('Georgia');
+   text("Greek Sculpture Garden", width/2, height/4);
+   
 
    fill(255, 255, 255);
-   text("Statue Garden", width/2, height/4);
-
-   fill(255, 255, 255);
+   textSize(30);
+   textFont('Helvetica');
    text("To enter click right arrow", width/2, height/3);
+
 }
 
-//-- drawTwo() will draw the image at index 1 from the array
+
 drawRoomOne = function() {
-   //image(images[1],width/2, height/2);
 
-   fill(240,120,0);
-   text("Room One", width/2, height - gTextOffset);
+   fill(130, 255, 160);
+   textSize(40);
+   textFont('Georgia');
+   text("The Archaic period", width/2, height/8);
+
+
+   fill(255, 255, 255);
+   textSize(23);
+   textFont('Helvetica');
+   text("In the archaic period (800-500 BC), the Greeks started to produce life-size statues inspired by Egyptian sculptures. They were made of stone and rigid in posture. The statues were famous for the Archaic smile seen here in the statues to the left.", width/2.5, 150, 400, 500);
+
+   //nav boxes 
+   fill(255, 255, 255);
+   textSize(30);
+   textFont('Georgia');
+   rect(navX, navY, navH, navW, 10);
+
+   fill(255, 255, 255);
+   rect(navX, navYb, navH, navW, 10);
+
+   fill(255, 255, 255);
+   rect(navX, navYc, navH, navW, 10);
+
+   //text for nav 
+   fill (0, 0, 0);
+   text("2", navNumx, navNumy);
+
+   fill (0, 0, 0);
+   text("3", navNumx, navNumb);
+
+   fill (0, 0, 0);
+   text("4", navNumx, navNumc);
+
+  image(statueAssests[3], 150, 400); //statue
+  image(statueAssests[4], 470, 580); //bust
+
+
 }
 
-//-- drawOne() will draw the image at index 2 from the array
 drawRoomTwo = function() {
    //image(images[2],width/2, height/2);
 
-   fill(255,20,147);
-   text("Room Two", width/2, height - gTextOffset);
+   fill(255, 20, 147);
+   textSize(45);
+   textFont('Georgia');
+   text("The Classical period", width/1.5, height/8 - gTextOffset);
+
+   fill(255, 255, 255);
+   textSize(20);
+   textFont('Helvetica');
+   text("In the Classical period (500-330 BC) Athens was at the height of culture and art. The statues in this time were becoming more lifelike and more attention was given to how the body moves. The statue to the left is called the Discus Thrower, it is famous for its original bronze casting. This stone version was recreated by the Romans. The figure is positioned right in the moment of throwing the discus. Classically, the figure has an emotionless face.", width/2, 150, 400, 500);
+
+   fill(255, 255, 255);
+   rect(navX, navY, navH, navW, 10);
+
+   fill(255, 255, 255);
+   rect(navX, navYb, navH, navW, 10);
+
+   //text for nav 
+
+   fill (0, 0, 0);
+   textSize(30);
+   textFont('Georgia');
+   text("3", navNumx, navNumy);
+
+   fill (0, 0, 0);
+   textSize(30);
+   textFont('Georgia');
+   text("4", navNumx, navNumb);
+
+   image(statueAssests[5], 200, 400);
+
+   
 }
 
-//-- drawOne() will draw the image at index 3 from the array
 drawRoomThree = function() {
-   //(images[3],width/2, height/2);
 
-   fill(30,144,255);
-   text("Room Three", width/2, height - gTextOffset);
+   fill(30, 144, 255);
+   textSize(40);
+   textFont('Georgia');
+   text("The Hellenistic period", width/1.5, height/8 - gTextOffset);
+
+   fill(255, 255, 255);
+   textSize(23);
+   textFont('Helvetica');
+   text("The statues in the Hellenistic period showed more realistic representations of the human body, including the injuries and unidealized bodies. The Venus De Milo (pictured below) is known for its missing limbs and mysterious absence of attributes. It is though to depict Aphrodite, the Greek goddess of love.", width/2, 120, 400, 900);
+
+   fill(255, 255, 255);
+   rect(navX, navY, navH, navW, 10);
+
+   fill(255, 255, 255);
+   rect(navX, navYb, navH, navW, 10);
+
+   //text for nav 
+
+   fill (0, 0, 0);
+   textSize(30);
+   textFont('Georgia');
+   text("4", navNumx, navNumy);
+
+   fill (0, 0, 0);
+   textSize(30);
+   textFont('Georgia');
+   text("2", navNumx, navNumb);
+
+   image(statueAssests[1], 160, 350); // not venus
+   image(statueAssests[2], 430, 540); //venus
+
+   
 }
 
 drawRoomFour = function() {
-   //(images[3],width/2, height/2);
 
-   fill(30,144,255);
-   text("Room Four", width/2, height - gTextOffset);
+   fill(30, 144, 255);
+   textSize(40);
+   textFont('Georgia');
+   text("The Hellenistic period", width/2, height/8);
+
+   fill(255, 255, 255);2
+   rect(navX, navY, navH, navW, 10);
+
+   fill(255, 255, 255);
+   rect(navX, navYb, navH, navW, 10);
+
+   //text for nav 
+
+   fill (0, 0, 0);
+   textSize(30);
+   textFont('Georgia');
+   text("5", navNumx, navNumy);
+
+   fill (0, 0, 0);
+   textSize(30);
+   textFont('Georgia');
+   text("1", navNumx, navNumb);
+
+   image(statueAssests[0], 475, 500);
 }
 
 drawEnding = function() {
-   //(images[3],width/2, height/2);
 
    fill(255, 255, 255);
    text("Exit", width/2, height/4);
 
    fill(255, 255, 255 );
-   text("Thanks for visiting", width/2, height/4);
+   textFont('Georgia');
+   text("Thanks for visiting", width/2, height/3);
+
+   fill(255, 255, 255);
+   rect(navX, navY, navH, navW, 10);
+
+   fill (0, 0, 0);
+   text("1", navNumx, navNumy);
+
 }
 
-//-- Will draw the current instuctions on the screen
-// function drawInstructions() {
-//   text("Bathroom", width/2, height - gTextOffset);
-// }
 
-
-//========= TEMPLATE: add or change interface functions, as you like =========
-
-// Change the drawFunction variable based on your interaction
 function keyPressed() {
-  // shows which was types
-  //print(key);
   print(keyCode);
 
-  // Entrance [] 
+  // Entrance [right arrow] 
   if( drawFunction === drawEntrance ) {
     if( keyCode === RIGHT_ARROW ) {
         drawFunction = drawRoomOne;
@@ -145,14 +252,14 @@ function keyPressed() {
 
   // RoomOne [1]
   else if( drawFunction === drawRoomOne ) {
-    if( key === '1' ) {
+    if( key === '2' ) {
       drawFunction = drawRoomTwo;
     }
-    if( key === '2' ) {
+    if( key === '3' ) {
       drawFunction = drawRoomThree;
     }
-    else if( key === '3' ) {
-      drawFunction = drawRoomThree;
+    else if( key === '4' ) {
+      drawFunction = drawRoomFour;
     }
   }
 
@@ -179,7 +286,7 @@ function keyPressed() {
   // RoomFour [4]
   else if( drawFunction === drawRoomFour ) {
    
-    if( key === '4') {
+    if( key === '5') {
       drawFunction = drawEnding;
     }
     if( key === '1' ) {
